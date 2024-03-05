a7a8477fd48dc0f0984b7d58a74148069f8950b5
DiiraC25 <162031366+DiiraC25@users.noreply.github.com>
2024-03-05 23:27:14 +0000

    Resolución pregunta 5

---
+++ Pregunta5.py
@@ -0,0 +1,5 @@
+shows = int(input("Por favor, ingrese cuántos shows musicales ha visto en el último año: "))
+if shows > 3:
+    print("Usted ha visto más de 3 shows musicales el último año")
+else:
+    print("Usted ha visto 3 o menos shows musicales el último año")
