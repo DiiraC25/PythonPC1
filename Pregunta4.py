1ec3fec7aa41785a9913b6fde0497bdcfb98d4eb
DiiraC25 <162031366+DiiraC25@users.noreply.github.com>
2024-03-05 23:21:55 +0000

    Resolución pregunta 4

--- Pregunta4.py
+++ Pregunta4.py
@@ -1,3 +1,3 @@
 N = int(input("Por favor, escriba un número entero positivo: "))
 sum = (N * (N+1))/2
-print(f"La suma de todos los enteros desde 1 hasta {N} es: {sum}")
+print(f"La suma de todos los enteros desde 1 hasta {N} es de {sum}")
