2e232127cb3f58f57ba43f8687ff826e7f9fa154
DiiraC25 <162031366+DiiraC25@users.noreply.github.com>
2024-03-05 23:16:13 +0000

    Resolución pregunta 3

---
+++ Pregunta3.py
@@ -0,0 +1,4 @@
+p = int(input("Inserte la cantidad de payasos solicitados: "))
+m = int(input("Inserte la cantidad de muñecas solicitadas: "))
+peso = (float(p*112+m*75))/1000
+print(f"El peso total del paquete que será enviado es de {peso} kg.")
---
+++ Pregunta4.py
@@ -0,0 +1,3 @@
+N = int(input("Por favor, escriba un número entero positivo: "))
+sum = (N * (N+1))/2
+print(f"La suma de todos los enteros desde 1 hasta {N} es: {sum}")
