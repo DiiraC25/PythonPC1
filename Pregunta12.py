facc3bc5dffe3c939881e338f83501e50c6e9bba
DiiraC25 <162031366+DiiraC25@users.noreply.github.com>
2024-03-06 20:54:21 +0000

    Resolución pregunta 12

--- Pregunta12.py
+++ Pregunta12.py
@@ -1,4 +1,4 @@
-entrada = input("Por favor, ingrese los nombres de los archivos junto con sus respectivas extensiones, en el siguiente formato: 'nombredearchivo.extension', y separelos por espacios: ")
+entrada = input("Por favor, ingrese los nombres de los archivos junto con sus respectivas extensiones, en el siguiente formato: 'nombredearchivo.extension', y separelos por espacios (solo se admiten las extensiones: .gif .jpg .jpeg .png .pdf .txt .zip): ")
 archivos = entrada.split()
 extensiones = ['jpg', 'jpeg', 'pdf', 'txt', 'png', 'zip', 'gif']
 tipos_mime = ['image/jpeg', 'image/jpeg', 'application/pdf', 'text/plain', 'image/png', 'application/zip', 'image/gif']
