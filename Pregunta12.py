2bca59a57963c9b4dda64fda58a3f158efec13d7
DiiraC25 <162031366+DiiraC25@users.noreply.github.com>
2024-03-06 20:43:55 +0000

    Resolución pregunta 12

---
+++ Pregunta12.py
@@ -0,0 +1,15 @@
+entrada = input("Por favor, ingrese los nombres de los archivos junto con sus respectivas extensiones, en el siguiente formato: 'nombredearchivo.extension', y separelos por espacios: ")
+archivos = entrada.split()
+extensiones = ['jpg', 'jpeg', 'pdf', 'txt', 'png', 'zip']
+tipos_mime = ['image/jpeg', 'image/jpeg', 'application/pdf', 'text/plain', 'image/png', 'application/zip']
+extensiones_archivos = [archivo.lower().split('.')[-1] for archivo in archivos]
+tipos_mime_archivos = []
+for extension in extensiones_archivos:
+    if extension in extensiones:
+        indice = extensiones.index(extension)
+        tipo_mime = tipos_mime[indice]
+        tipos_mime_archivos.append(tipo_mime)
+    else:
+        tipos_mime_archivos.append('application/octet-stream')
+for archivo, tipo_mime in zip(archivos, tipos_mime_archivos):
+    print(f"Nombre Archivo: {archivo} - Salida Esperada: {tipo_mime}")
