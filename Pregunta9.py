d73a3a6a62bfe1742548f34355de682ad3515f23
DiiraC25 <162031366+DiiraC25@users.noreply.github.com>
2024-03-06 01:53:06 +0000

    Resolución pregunta 9

--- Pregunta9.py
+++ Pregunta9.py
@@ -1,3 +1,5 @@
-lista = ['Di', 'buen', 'día', 'a', 'papa']
-lista.reverse()
-print(lista)
+entrada = input("Por favor ingrese palabras separadas por espacios, para formar una lista: ")
+lista = entrada.split()
+lista1 = entrada.split()
+lista1.reverse()
+print(f"La lista original es {lista} y la invertida es {lista1}")
